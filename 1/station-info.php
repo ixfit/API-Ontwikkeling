0a8afec1c73ecc4432d653b906e9e61b4fc806a5
ixfit <50911487+ixfit@users.noreply.github.com>
2020-06-26 09:06:41 +0200

    2

---
+++ 1/station-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$url = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/arrivals?station=" . $user=$_POST['station'];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_POSTREDIR, 3);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // follow http 3xx redirects
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Ocp-Apim-Subscription-Key: 3113d4a1eb0641e4b68034952e5b5bd0'
+));
+
+$resp_orders = curl_exec($ch);
+
+$json = json_decode($resp_orders);
+
+//echo $resp_orders;
+
+foreach ($json-> payload as $station){
+    echo$station->station . "<br>";
+    echo$station->dateTime . "<br>";
+//    echo$station->namen->lang . "<br>";
+}
